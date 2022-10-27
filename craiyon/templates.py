fe44b19b1d0a366230cc984528a290dca217f73f
mdm9300404 <92705934+mdm9300404@users.noreply.github.com>
2022-10-27 00:06:52 -0400

    Update templates.py

--- craiyon/templates.py
+++ craiyon/templates.py
@@ -1,17 +1,18 @@
 import base64
 from pathlib import Path
+import aiofiles
 
 class GeneratedImages:
     def __init__(self, images: dict):
         self.images = images
     
-    def save_images(self, path: str=None):
+    async def save_images(self, path: str=None):
         '''
         Saves the generated images to the given path.
         Defaults to cwd/generated
         '''
         path = (Path.cwd() / 'generated') if not path else Path(path)
         path.mkdir(parents=True, exist_ok=True)
-        for i in enumerate(self.images):
-            with open(path / f'image-{i[0]+1}.png', 'wb') as f:
-                f.write(base64.decodebytes(i[1].encode('utf-8')))
+        for i in enumerate(self.images['images']):
+            async with aiofiles.open(path / f'image-{i[0]+1}.png', 'wb') as f:
+                await f.write(base64.decodebytes(i[1].encode('utf-8')))
