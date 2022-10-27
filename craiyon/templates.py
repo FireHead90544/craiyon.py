c49d27e071f6b7eb880e40a973d3c795ab22e90b
Linus <92705934+mdm9300404@users.noreply.github.com>
2022-10-27 16:08:25 -0400

    Update templates.py

--- craiyon/templates.py
+++ craiyon/templates.py
@@ -5,8 +5,8 @@ import aiofiles
 class GeneratedImages:
     def __init__(self, images: dict):
         self.images = images
-    
-    async def save_images(self, path: str=None):
+        
+    def save_images(self, path: str=None):
         '''
         Saves the generated images to the given path.
         Defaults to cwd/generated
@@ -14,5 +14,17 @@ class GeneratedImages:
         path = (Path.cwd() / 'generated') if not path else Path(path)
         path.mkdir(parents=True, exist_ok=True)
         for i in enumerate(self.images['images']):
-            async with aiofiles.open(path / f'image-{i[0]+1}.png', 'wb') as f:
+            with open(path / f'image-{i[0]+1}.jpg', 'wb') as f:
+                f.write(base64.decodebytes(i[1].encode('utf-8')))
+    
+    async def async_save_images(self, path: str=None):
+        '''
+        Saves the generated images to the given path.
+        Defaults to cwd/generated.
+        Async version not yet complete; Still has some blocking code.
+        '''
+        path = (Path.cwd() / 'generated') if not path else Path(path)
+        path.mkdir(parents=True, exist_ok=True)
+        for i in enumerate(self.images['images']):
+            async with aiofiles.open(path / f'image-{i[0]+1}.jpg', 'wb') as f:
                 await f.write(base64.decodebytes(i[1].encode('utf-8')))
