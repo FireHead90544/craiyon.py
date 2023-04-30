121897b9ccdcfee7c38dbd61ef78498dc67c085c
FireHead90544 <rudranshjoshi1806@gmail.com>
2023-04-30 23:43:08 +0530

    Released v1.0.0

--- craiyon/craiyon.py
+++ craiyon/craiyon.py
@@ -1,10 +1,10 @@
 from __future__ import annotations
 import aiohttp
 import requests
-from craiyon.templates import GeneratedImagesV1, GeneratedImagesV2
+from craiyon.templates import GeneratedImages
 
 
-# V2 version of Craiyon API
+# v3 version of Craiyon API
 class Craiyon:
     '''
     Instantiates a Craiyon session, allows user to generate images from text prompts.
@@ -14,84 +14,163 @@ class Craiyon:
     Arguments:        
         - (Not required) | api_token: Useful if you're paying for a subscription on Craiyon's website and want to remove the watermark from the generated images
         
-        - (Not required) | model_version: Since Craiyon is constantly updating their V2 model seemingly everyday, you can specify a newer model here.
+        - (Not required) | model_version: Since Craiyon is constantly updating their v3 model seemingly everyday, you can specify a newer model here.
         Defaults to \"35s5hfwn9n78gb06\" (March 10th Model) if nothing is specified here
-        
     '''
+    
     def __init__(self, api_token=None, model_version="35s5hfwn9n78gb06") -> None:
         self.BASE_URL = "https://api.craiyon.com"
-        self.DRAW_API_ENDPOINT = "/draw"
+        self.DRAW_API_ENDPOINT = "/v3"
         self.model_version = model_version
         self.api_token = api_token
-    
-    # Generate images with V2
-    def generate(self, prompt: str) -> GeneratedImagesV2:
-        
+
+    # Generate images with v3
+    def generate(self, prompt: str, negative_prompt: str = "", model_type: str = "none") -> GeneratedImages:
         """
-        Generates 9 images using the V2 model of Craiyon.
+        Generates 9 images using the v3 model of Craiyon.
         
         Arguments:
-        - (Required) | Prompt: The text prompt that will be used to generate the images
+        - (Required) | prompt: The text prompt that will be used to generate the images
+
+        - (Not Required) | negative_prompt: What you don't want to see in the images
+        Defaults to "" (Let the model decide)
+
+        - (Not Required) | model_type: The type of images to be generated
+        Defaults to "none" (Let the model decide), available options include "art", "drawing", "photo", "none"
 
         Returns:
-        - Returns a list of direct image links to `.webp` images, from https://img.craiyon.com
+        - Returns a list of direct .webp image links from https://img.craiyon.com and a Description about the generated images
         """
-            
+
         url = self.BASE_URL + self.DRAW_API_ENDPOINT
         session = requests.Session()
-        resp = session.post(url, json={'prompt': prompt, "token": self.api_token, "version": self.model_version})
-        
+        resp = session.post(url, json={'prompt': prompt, "negative_prompt": negative_prompt, "model": model_type, "token": self.api_token, "version": self.model_version})
         resp = resp.json()
-                
+
         # Add protocol, domain and subdomain (https://img.craiyon.com) to each item as those aren't included in the response by default
         images = [f"https://img.craiyon.com/{item}" for item in resp['images']]
-        
-        return GeneratedImagesV2(images)
+
+        return GeneratedImages(images, description=resp["next_prompt"], model="v3")
     
-    # Generate images with V2, asynchronously
-    async def async_generate(self, prompt: str) -> GeneratedImagesV2:
-        
+    # Generate images with v3, asynchronously
+    async def async_generate(self, prompt: str, negative_prompt: str = "", model_type: str = "none") -> GeneratedImages:
         """
-        Generates 9 images asynchronously using the V2 model of Craiyon.
+        Generates 9 images asynchronously using the v3 model of Craiyon.
         
         Arguments:
-        - (Required) | Prompt: The text prompt that will be used to generate the images
+        - (Required) | prompt: The text prompt that will be used to generate the images
+
+        - (Not Required) | negative_prompt: What you don't want to see in the images
+        Defaults to "" (Let the model decide)
+
+        - (Not Required) | model_type: The type of images to be generated
+        Defaults to "none" (Let the model decide), available options include "art", "drawing", "photo", "none"
 
         Returns:
-        - Returns a list of direct image links to `.webp` images, from https://img.craiyon.com
+        - Returns a list of direct .webp image links from https://img.craiyon.com and a Description about the generated images
         """
-        
+
         url = self.BASE_URL + self.DRAW_API_ENDPOINT
         async with aiohttp.ClientSession() as sess:
-            async with sess.post(url, json={'prompt': prompt, "token": self.api_token, "version": self.model_version}) as resp:
-                urls_no_domain = await resp.json()
+            async with sess.post(url, json={'prompt': prompt, "negative_prompt": negative_prompt, "model": model_type, "token": self.api_token, "version": self.model_version}) as resp:
+                resp = await resp.json()
                 # Add protocol, domain and subdomain (https://img.craiyon.com) to each item as those aren't included in the response by default
-                images = [f"https://img.craiyon.com/{item}" for item in urls_no_domain['images']]
+                images = [f"https://img.craiyon.com/{item}" for item in resp['images']]
         
-                return GeneratedImagesV2(images)
-    
-# V1 version of Craiyon API, for backwards compatibility
+                return GeneratedImages(images, description=resp["next_prompt"], model="v3")
+
+
+# v2 version of Craiyon API, deprecated (removed completely)
+# class CraiyonV2:
+#     '''
+#     **NOTICE**: This is the v2 version of Craiyon's model. This version is deprecated as the api endpoint is completely removed, so it won't work at all. This code is commented out just in hopes of the api endpoint getting back. To use the latest model, use the normal `Craiyon` class instead.
+#
+#     Instantiates a Craiyon session, allows user to generate images from text prompts.
+#     The model takes some time to generate the images (roughly around 1 minute).
+#     So be patient, and don't abuse the api, as I am not the one hosting the model, craiyon itself is.
+#    
+#     Arguments:        
+#         - (Not required) | api_token: Useful if you're paying for a subscription on Craiyon's website and want to remove the watermark from the generated images
+#        
+#         - (Not required) | model_version: Since Craiyon is constantly updating their v2 model seemingly everyday, you can specify a newer model here.
+#         Defaults to \"35s5hfwn9n78gb06\" (March 10th Model) if nothing is specified here
+#        
+#     '''
+#
+#     def __init__(self, api_token=None, model_version="35s5hfwn9n78gb06") -> None:
+#         self.BASE_URL = "https://api.craiyon.com"
+#         self.DRAW_API_ENDPOINT = "/draw"
+#         self.model_version = model_version
+#         self.api_token = api_token
+#    
+#     # Generate images with v2
+#     def generate(self, prompt: str) -> GeneratedImages:
+#         """
+#         Generates 9 images using the v2 model of Craiyon.
+#        
+#         Arguments:
+#         - (Required) | prompt: The text prompt that will be used to generate the images
+#
+#         Returns:
+#         - Returns a list of direct image links to `.webp` images, from https://img.craiyon.com
+#         """
+#            
+#         url = self.BASE_URL + self.DRAW_API_ENDPOINT
+#         session = requests.Session()
+#         resp = session.post(url, json={'prompt': prompt, "token": self.api_token, "version": self.model_version})
+#        
+#         resp = resp.json()
+#                
+#         # Add protocol, domain and subdomain (https://img.craiyon.com) to each item as those aren't included in the response by default
+#         images = [f"https://img.craiyon.com/{item}" for item in resp['images']]
+#        
+#         return GeneratedImages(images, model="v2")
+#    
+#     # Generate images with v2, asynchronously
+#     async def async_generate(self, prompt: str) -> GeneratedImages:
+#        
+#         """
+#         Generates 9 images asynchronously using the v2 model of Craiyon.
+#        
+#         Arguments:
+#         - (Required) | prompt: The text prompt that will be used to generate the images
+#
+#         Returns:
+#         - Returns a list of direct image links to `.webp` images, from https://img.craiyon.com
+#         """
+#        
+#         url = self.BASE_URL + self.DRAW_API_ENDPOINT
+#         async with aiohttp.ClientSession() as sess:
+#             async with sess.post(url, json={'prompt': prompt, "token": self.api_token, "version": self.model_version}) as resp:
+#                 urls_no_domain = await resp.json()
+#                 # Add protocol, domain and subdomain (https://img.craiyon.com) to each item as those aren't included in the response by default
+#                 images = [f"https://img.craiyon.com/{item}" for item in urls_no_domain['images']]
+#        
+#                 return GeneratedImages(images, model="v2")
+#
+
+
+# v1 version of Craiyon API, for backwards compatibility
 class CraiyonV1:
     '''
-    **NOTICE**: This is the V1 version of Craiyon's model. To use the updated V2 model, use the normal `Craiyon` class instead.
+    **NOTICE**: This is the v1 version of Craiyon's model. To use the latest model, use the normal `Craiyon` class instead.
     
     Instantiates a Craiyon session, allows user to generate images from text prompts.
     The model takes some time to generate the images (roughly around 1 minute).
     So be patient, and don't abuse the api, as I am not the one hosting the model, craiyon itself is.
     '''
-    
-    
+ 
     def __init__(self) -> None:
         self.BASE_URL = "https://backend.craiyon.com"
         self.DRAW_API_ENDPOINT = "/generate"
     
-    def generate(self, prompt: str) -> GeneratedImagesV1:
+    def generate(self, prompt: str) -> GeneratedImages:
         
         """
-        Generates 9 images using the V1 model of Craiyon.
+        Generates 9 images using the v1 model of Craiyon.
         
         Arguments:
-        - (Required) | Prompt: The text prompt that will be used to generate the images
+        - (Required) | prompt: The text prompt that will be used to generate the images
         
         Returns:
         - Returns a list of 9 Base64 bytestrings (.jpg)
@@ -100,15 +179,15 @@ class CraiyonV1:
         session = requests.Session()
         url = self.BASE_URL + self.DRAW_API_ENDPOINT
         resp = session.post(url, json={'prompt': prompt})
-        return GeneratedImagesV1(resp.json()['images'])
+        return GeneratedImages(resp.json()['images'], model="v1")
 
-    async def async_generate(self, prompt: str) -> GeneratedImagesV1:
+    async def async_generate(self, prompt: str) -> GeneratedImages:
         
         """
-        Generates 9 images asynchronously using the V1 model of Craiyon.
+        Generates 9 images asynchronously using the v1 model of Craiyon.
         
         Arguments:
-        - (Required) | Prompt: The text prompt that will be used to generate the images
+        - (Required) | prompt: The text prompt that will be used to generate the images
         
         Returns:
         - Returns a list of 9 Base64 bytestrings (.jpg)
@@ -118,4 +197,4 @@ class CraiyonV1:
         async with aiohttp.ClientSession() as sess:
             async with sess.post(url, json={"prompt": prompt}) as resp:
                 resp = await resp.json()
-                return GeneratedImagesV1(resp['images'])
+                return GeneratedImages(resp['images'], model="v1")
--- craiyon/templates.py
+++ craiyon/templates.py
@@ -5,77 +5,53 @@ from aiopath import AsyncPath
 from craiyon.craiyon_utils import encode_base64, async_encode_base64
 
 
-# Handles V2 version of Craiyon API
-class GeneratedImagesV2:
-    def __init__(self, images: list):
+# Handles the images returned from the Craiyon API
+class GeneratedImages:
+    def __init__(self, images: list, description: str = None, model: str = None):
         self.images = images
-            
-    # Save Images  
-    def save_images(self, path: str=None):
-        '''
+        self.description = description
+        self.model = model
+
+    def save_images(self, path: str = None):
+        """
         Saves the generated images to the given path.
         Defaults to cwd/generated
-        
-        Filetype saved: `.webp`
-        '''
-        
-        # Get list of bytestring objects
-        image_list = encode_base64(self.images)
-        # Make new directory
-        path = (Path.cwd() / 'generated') if not path else Path(path)
-        path.mkdir(parents=True, exist_ok=True)
-        # Save each image to specified path
-        for i in enumerate(image_list):
-            with open(path / f'image-{i[0]+1}.webp', 'wb') as f:
-                f.write(base64.decodebytes(i[1])) # Save file to disk
-    
-    # Async Save Images
-    async def async_save_images(self, path: str=None):
-        '''
-        Asynchronously saves the generated images to the given path.
-        Defaults to cwd/generated.
-        
-        Filetype saved: `.webp`
-        '''
-        
-        # Get list of bytestring objects
-        image_list = await async_encode_base64(self.images)
-        # Make new directory
-        path = (AsyncPath.cwd() / 'generated') if not path else AsyncPath(path)
-        await path.mkdir(parents=True, exist_ok=True)
-        # Save each image to specified path
-        for i in enumerate(image_list):
-            async with aiofiles.open(path / f'image-{i[0]+1}.webp', 'wb') as f:
-                await f.write(base64.decodebytes(i[1])) # Save file to disk
 
+        Filetype saved: `.jpg` for v1, '.webp' for v3
+        """
+        images = self.images
+        extension = "jpg"
+        if self.model in ["v2", "v3"]:
+            images = encode_base64(self.images)
+            extension = "webp"
 
-# Handles V1 version of Craiyon API
-class GeneratedImagesV1:
-    def __init__(self, images: list):
-        self.images = images
-        
-    def save_images(self, path: str=None):
-        '''
-        Saves the generated images to the given path.
-        Defaults to cwd/generated
-        
-        Filetype saved: `.jpg`
-        '''
-        path = (Path.cwd() / 'generated') if not path else Path(path)
+        path = (Path.cwd() / "generated") if not path else Path(path)
         path.mkdir(parents=True, exist_ok=True)
-        for i in enumerate(self.images):
-            with open(path / f'image-{i[0]+1}.jpg', 'wb') as f:
-                f.write(base64.decodebytes(i[1].encode('utf-8')))
-    
-    async def async_save_images(self, path: str=None):
-        '''
+        for i in enumerate(images):
+            with open(path / f"image-{i[0]+1}.{extension}", "wb") as f:
+                if self.model == "v1":
+                    f.write(base64.decodebytes(i[1].encode("utf-8")))
+                else:
+                    f.write(base64.decodebytes(i[1]))
+
+    async def async_save_images(self, path: str = None):
+        """
         Saves the generated images to the given path.
         Defaults to cwd/generated.
-        
+
         Filetype saved: `.jpg`
-        '''
-        path = (AsyncPath.cwd() / 'generated') if not path else AsyncPath(path)
+        """
+        images = self.images
+        extension = "jpg"
+        if self.model in ["v2", "v3"]:
+            images = await async_encode_base64(self.images)
+            extension = "webp"
+
+        path = (AsyncPath.cwd() / "generated") if not path else AsyncPath(path)
         await path.mkdir(parents=True, exist_ok=True)
-        for i in enumerate(self.images):
-            async with aiofiles.open(path / f'image-{i[0]+1}.jpg', 'wb') as f:
-                await f.write(base64.decodebytes(i[1].encode('utf-8')))
+        for i in enumerate(images):
+            async with aiofiles.open(path / f"image-{i[0]+1}.{extension}", "wb") as f:
+                if self.model == "v1":
+                    await f.write(base64.decodebytes(i[1].encode("utf-8")))
+                else:
+                    await f.write(base64.decodebytes(i[1]))
